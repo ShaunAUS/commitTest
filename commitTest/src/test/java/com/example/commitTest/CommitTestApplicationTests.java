a50aea7e19cee8e660c3fd681220d9675736f22e
Shaun <86937655+ShaunAUS@users.noreply.github.com>
2022-07-04 11:46:35 +0900

    Add files via upload

---
+++ commitTest/src/test/java/com/example/commitTest/CommitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commitTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
